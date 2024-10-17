75137774d4d5af2134abfc5a9adbd667cddb8f6a
chibuezeozojideofor@gmail.com <Peter_Manasseh_Oz>
2024-10-17 18:03:03 +0100

    UI of chat box complete with RightSidebar.jsx

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -7,9 +7,22 @@ const RightSidebar = () => {
     <div className="rs">
         <div className="rs-profile">
             <img src={assets.profile_img} alt="" />
-            <h3>Richard Dixon <img src={assets.green_dot} className="dot" alt="" /></h3>
-            <p>Hey there, I am using Empire</p>
+            <h3>Richard Nixon <img src={assets.green_dot} className="dot" alt="" /></h3>
+            <p>Hey there, I am Richard Nixon, and I am using Empire.</p>
         </div>
+        <hr />
+        <div className="rs-media">
+            <p>Media</p>
+            <div>
+                <img src={assets.pic1} alt="" />
+                <img src={assets.pic2} alt="" />
+                <img src={assets.pic3} alt="" />
+                <img src={assets.pic4} alt="" />
+                <img src={assets.pic1} alt="" />
+                <img src={assets.pic2} alt="" />
+            </div>
+        </div>
+        <button>Logout</button>
     </div>
     );
 };
